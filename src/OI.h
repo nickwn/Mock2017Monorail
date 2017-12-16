9053e91cc35eb6d5c1c49e29d1830248d38cffc9
LAPTOP-A4KE1I09\Austin <austinmingli@gmail.com>
2017-12-15 20:44:09 -0800

    removed test buttons, added joystick for arm

--- src/OI.h
+++ src/OI.h
@@ -6,12 +6,7 @@ class OI {
 private:
 	Joystick* leftStick;
 	Joystick* rightStick;
-<<<<<<< HEAD
 	Joystick* ArmStick;
-=======
-	Button* gyroTurn;
-	Button* drive;
->>>>>>> 519c26f6f774607e08e3c5b87fd1f8dc16898650
 public:
 	OI();
 	Joystick* getLeft();
