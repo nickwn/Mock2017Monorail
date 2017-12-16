38db473053fd04eb844f8f51b97eb1be7f3df6ac
LAPTOP-A4KE1I09\Austin <austinmingli@gmail.com>
2017-12-15 20:41:48 -0800

    commands to move arm

--- src/Commands/ArmDown.cpp
+++ src/Commands/ArmDown.cpp
@@ -3,21 +3,22 @@
 ArmDown::ArmDown() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
+	Requires(arm);
 }
 
 // Called just before this Command runs the first time
 void ArmDown::Initialize() {
-
+	SetTimeout(0.5);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void ArmDown::Execute() {
-
+	arm->armD(-0.2);
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool ArmDown::IsFinished() {
-	return false;
+	return IsTimedOut();
 }
 
 // Called once after isFinished returns true
--- src/Commands/ArmUp.cpp
+++ src/Commands/ArmUp.cpp
@@ -3,21 +3,22 @@
 ArmUp::ArmUp() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
+	Requires(arm);
 }
 
 // Called just before this Command runs the first time
 void ArmUp::Initialize() {
-
+	SetTimeout(0.5);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void ArmUp::Execute() {
-
+	arm->armD(0.2);
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool ArmUp::IsFinished() {
-	return false;
+	return IsTimedOut();
 }
 
 // Called once after isFinished returns true
